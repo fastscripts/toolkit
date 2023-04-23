1969805156ade60bcb9d0636e34959bdb6a0e8a1
unknown <you@example.com>
2023-04-23 13:13:30 +0200

    Lektion 6 generating slags

--- tools_test.go
+++ tools_test.go
@@ -168,3 +168,31 @@ func TestTools_CreateDirIfNotExist(t *testing.T) {
 	}
 	_ = os.Remove("./testdata/myDir")
 }
+
+var slugTests = []struct {
+	name          string
+	s             string
+	expected      string
+	errorExpected bool
+}{
+	{name: "valid string", s: "now is the time", expected: "now-is-the-time", errorExpected: false},
+	{name: "empty string", s: "", expected: "", errorExpected: true},
+	{name: "complex string", s: "Now is the time for all GOOD men! + fish such &^123", expected: "now-is-the-time-for-all-good-men-fish-such-123", errorExpected: false},
+	{name: "Cyrillic string", s: "Привет, как дела?", expected: "", errorExpected: true},
+	{name: "Cyrillic and Roman string", s: "Привет, как дела? Hallo, wie geht es dir?", expected: "hallo-wie-geht-es-dir", errorExpected: false},
+}
+
+func TestTools_Slugify(t *testing.T) {
+	var testTool Tools
+
+	for _, e := range slugTests {
+		slug, err := testTool.Slugify(e.s)
+		if err != nil && !e.errorExpected {
+			t.Errorf("%s: error recevied when none expected: %s", e.name, err.Error())
+		}
+		if !e.errorExpected && slug != e.expected {
+			t.Errorf("%s: wrong slug returned; expected %s but got %s", e.name, e.expected, slug)
+		}
+	}
+
+}
